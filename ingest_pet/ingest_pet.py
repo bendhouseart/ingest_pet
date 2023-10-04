a391b5c529bd69186234e1a5183b65c3f5d9c517
Anthony Galassi <28850131+bendhouseart@users.noreply.github.com>
2023-10-03 21:06:45 -0400

    this may not be a bad way to go about it

--- ingest_pet/ingest_pet.py
+++ ingest_pet/ingest_pet.py
@@ -5,9 +5,10 @@ import pathlib
 import json
 import pytest
 import tempfile
-from bids import layout
-
-
+from bids import BIDSLayout
+from bids.layout.models import BIDSImageFile, BIDSJSONFile
+from typing import Union
+from difflib import get_close_matches
 
 
 def get_versions():
@@ -63,25 +64,50 @@ def write_out_dataset_description_json(input_bids_dir, output_bids_dir=None):
 
     with open(os.path.join(output_bids_dir, 'dataset_description.json'), 'w') as f:
         dataset_description = {
-            "Name": f"petdeface - PET and Anatomical Defacing workflow: "
-                    f"PET Defaced Version of BIDS Dataset `{source_dataset_description['Name']}`",
+            "Name": f"description verygeneric - this is a placeholder: "
+                    f"Not much to read here, if this has been published you've messed up`{source_dataset_description['Name']}`",
             "BIDSVersion": __bids_version__,
             "GeneratedBy": [
-                {"Name": "PET Deface",
+                {"Name": "TBD",
                  "Version": __version__,
-                 "CodeURL": "https://github.com/bendhouseart/petdeface"}],
-            "HowToAcknowledge": "This workflow uses FreeSurfer: `Fischl, B., FreeSurfer. Neuroimage, 2012. 62(2): p. 774-8.`,"
-                                "and the MiDeFace package developed by Doug Greve: `https://surfer.nmr.mgh.harvard.edu/fswiki/MiDeFace`",
+                 "CodeURL": "https://github.com/someuser/someproject"}],
+            "HowToAcknowledge": "This ________ uses ______________: `Someone, A., A Title. Journal, 2099. 12(3): p. 1-5.`,"
+                                "and the ___________ developed by Some other person: `https://notarealurl.super.fake/extremelyfake`",
             "License": "CCBY"
         }
 
         json.dump(dataset_description, f, indent=4)
 
-def collect_anat(bids_data: pathlib.Path, suffix="T1w"):
+def collect_anat_and_pet(bids_data: Union[pathlib.Path, BIDSLayout], suffixes=["T1w", "T2w"], subjects: list=[]):
+    if type(bids_data) is BIDSLayout:
+        pass
+    elif isinstance(bids_data, (pathlib.PosixPath, pathlib.WindowsPath)) and bids_data.exists():
+        bids_data = BIDSLayout(bids_data)
+    else:
+        raise TypeError(f"{bids_data} must be a BIDSLayout or valid Path object, given type: {type(bids_data)}.")
+    
+    # return all subjects if no list of subjects is given
+    if subjects == []:
+        subjects = bids_data.get_subjects()
+
+    mapped_pet_to_anat = {}
+    for subject in subjects:
+        mapped_pet_to_anat[subject] = {}
+    for subject in subjects:
+        pet_files = bids_data.get(subject=subject, suffix="pet")
+        anat_files = [a.path for a in bids_data.get("anat",suffix=suffixes, subject=subject, extension=["nii", "nii.gz"])]
+        # for each pet image file we create an entry our mapping dictionary
+        for entry in pet_files:
+            if type(entry) is BIDSImageFile:
+                try:
+                    # search through anatomical files and find the closest match
+                    mapped_pet_to_anat[subject][entry.path] = get_close_matches(entry.path, anat_files, n=1)[0]
+                except IndexError:
+                    mapped_pet_to_anat[subject][entry.path] = ''
+    return mapped_pet_to_anat
+
+def collect_pet(bids_data: Union[pathlib.Path, BIDSLayout]):
     return None
 
-def collect_pet(bids_data: pathlib.Path):
-    return None
-
-def associte_anat_pet(bids_data: pathlib.Path):
+def associte_anat_pet(bids_data: Union[pathlib.Path, BIDSLayout]):
     return None
